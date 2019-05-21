3e6f68cde55d882393c122b38547c8e81894fab1
yyamada12 <12yacropolisy@gmail.com>
2019-05-21 22:53:07 +0900

    [add] navigation controller and memo view controller

--- MyMemoApp/MemoTableViewController.swift
+++ MyMemoApp/MemoTableViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class MemoTableViewController: UITableViewController {
+    
+    var memos = [
+        ["blue", "red", "pink"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,23 +28,24 @@ class MemoTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return memos.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoTableViewCell", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = memos[indexPath.row]
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
---
+++ MyMemoApp/MemoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoViewController.swift
+//  MyMemoApp
+//
+//  Created by Yoshinobu Yamada on 2019/05/21.
+//  Copyright © 2019 Yoshinobu Yamada. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
